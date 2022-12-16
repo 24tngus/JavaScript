b4b8119cc0b313d90ed9eb577660f7e1c9b17205
수현 <68477988+24tngus@users.noreply.github.com>
2022-12-16 22:40:25 +0900

    Update and rename programmers/pg1_1 문자열을 정수로 바꾸기.js to programmers/Lv1/pg1_1 문자열을 정수로 바꾸기.js

---
+++ programmers/Lv1/pg1_1 문자열을 정수로 바꾸기.js
@@ -0,0 +1,17 @@
+// 1. 문자열을 정수로 바꾸기 
+// 문자열 s를 숫자로 변환한 결과를 반환하는 함수, solution을 완성하세요.
+
+function solution(s) {
+    var answer = 0;
+    
+    // 1)
+    answer = Number(s)
+
+    // 2)
+    answer = s/1
+
+    // 3)
+    answer = +s
+    
+    return answer;
+}
